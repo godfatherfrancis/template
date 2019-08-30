2a4b3d8b42bd8e942a12ee21be247bb1013d50ec
highresfelix <highresfelix@gmail.com>
2019-08-30 02:37:01 -0400

    ItemInventory - add TODOs

--- ItemInventory/src/main/java/com/highresfelix/inventory/ItemTracker.java
+++ ItemInventory/src/main/java/com/highresfelix/inventory/ItemTracker.java
@@ -7,10 +7,30 @@ import java.util.Scanner;
  */
 
 public class ItemTracker {
+    public static InventoryItem inventory = new InventoryItem();
     public static Stocker stocker = new Stocker();
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         stocker.login();
+    }
+
+    // TODO return an object using the correct category class (or throws and error for an invalid category name).???
+    // TODO createItem to create a new item for option 1.
+    // TODO When you list the items, list their category as well.
+    static InventoryItem createItem(String name, int quantity, String category) {
+        System.out.println("Choose category:" +
+                "\n[1] Food" +
+                "\n[2] Hat" +
+                "\n[3] Shirt" +
+                "\n[4] Pants" +
+                "\n[5] Shoe");
+        int selection = Integer.parseInt(scanner.next());
+
+        if (category.equalsIgnoreCase("shoe")) {
+            return new Shoe(name, quantity);
+        } else {
+            return null;
+        }
     }
 }
