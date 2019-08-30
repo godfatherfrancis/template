f8e2c13d5070780979ef7797b3eba607efe1048a
highresfelix <highresfelix@gmail.com>
2019-08-30 01:24:10 -0400

    HelloWorld - ReadWriteJson updated

--- HelloWorld/src/main/java/com/highresflix/helloworld/ReadWriteJson.java
+++ HelloWorld/src/main/java/com/highresflix/helloworld/ReadWriteJson.java
@@ -1,8 +1,13 @@
 package main.java.com.highresflix.helloworld;
 
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Period;
+import java.util.Scanner;
 
 /**
  * created by @highresfelix on 8/30/19
@@ -17,5 +22,19 @@ public class ReadWriteJson {
         File file = new File("person.json");
 
         // write json
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.serialize(person);
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(json);
+        fileWriter.close();
+
+        // read json
+        Scanner scanner = new Scanner(file);
+        scanner.useDelimiter("\\Z");
+        String contents = scanner.next();
+        JsonParser parser = new JsonParser();
+        Person personTwo = parser.parse(contents, Person.class);
+
+        System.out.println(personTwo);
     }
 }
