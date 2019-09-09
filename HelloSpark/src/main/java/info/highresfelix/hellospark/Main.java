1d4494036922e220b084a5a9ee510f6ede9b57c5
highresfelix <highresfelix@gmail.com>
2019-09-09 00:32:11 -0400

    HelloSpark/ - added

---
+++ HelloSpark/src/main/java/info/highresfelix/hellospark/Main.java
@@ -0,0 +1,43 @@
+package main.java.info.highresfelix.hellospark;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
+/**
+ * created by @highresfelix on 9/9/19
+ */
+
+public class Main {
+    static User user;
+
+    public static void main(String[] args) {
+        Spark.init();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap hashMap = new HashMap();
+                    if (user == null) {
+                        return new ModelAndView(hashMap, "login.html");
+                    } else {
+                        hashMap.put("name", user.name);
+                        return new ModelAndView(hashMap, "home.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                ((request, response) -> {
+                    String name = request.queryParams("loginName");
+                    user = new User(name);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+    }
+}
